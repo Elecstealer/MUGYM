8546eaefb72bebf24b38ca30a9a37388f82ecf4c
ParkSieun <110987950+Elecstealer@users.noreply.github.com>
2025-01-24 14:10:54 +0900

    Update playlist_screen.dart

--- frontend/lib/screens/playlist_screen.dart
+++ frontend/lib/screens/playlist_screen.dart
@@ -4,7 +4,7 @@ import 'play_screen.dart';
 import 'package:http/http.dart' as http;
 import 'dart:convert';
 
-class PlaylistScreen extends StatelessWidget {
+class PlaylistScreen extends StatefulWidget {
   final String playlistName;
   final List<dynamic> tracks;
   final int userId;
@@ -15,21 +15,42 @@ class PlaylistScreen extends StatelessWidget {
     required this.playlistName,
     required this.tracks,
     required this.userId,
-    required this.mypageflag
+    required this.mypageflag,
   });
+
+  @override
+  State<PlaylistScreen> createState() => _PlaylistScreenState();
+}
+
+class _PlaylistScreenState extends State<PlaylistScreen> {
+  late String _playlistName;
+  final TextEditingController _textEditingController = TextEditingController();
+
+  @override
+  void initState() {
+    super.initState();
+    _playlistName = widget.playlistName;
+    _textEditingController.text = _playlistName;
+  }
+
+  @override
+  void dispose() {
+    _textEditingController.dispose();
+    super.dispose();
+  }
 
   void playAllTracks(BuildContext context) {
     Navigator.of(context).push(MaterialPageRoute(
       builder: (context) => PlayScreen(
-        playlistName: playlistName,
-        tracks: tracks,
+        playlistName: _playlistName,
+        tracks: widget.tracks,
       ),
     ));
   }
 
   void playRandomTrack(BuildContext context) {
-    if (tracks.isNotEmpty) {
-      final randomTrack = tracks[Random().nextInt(tracks.length)];
+    if (widget.tracks.isNotEmpty) {
+      final randomTrack = widget.tracks[Random().nextInt(widget.tracks.length)];
       Navigator.of(context).push(MaterialPageRoute(
         builder: (context) => PlayScreen(
           playlistName: '랜덤 트랙: ${randomTrack['track_name']}',
@@ -42,9 +63,9 @@ class PlaylistScreen extends StatelessWidget {
   Future<void> savePlaylist(BuildContext context) async {
     final url = Uri.parse('http://10.0.2.2:8000/auth/save_playlist/');
     final body = jsonEncode({
-      'userId': userId,
-      'exercise_type': playlistName,
-      'tracks': tracks.map((track) {
+      'userId': widget.userId,
+      'exercise_type': _playlistName,
+      'tracks': widget.tracks.map((track) {
         return {
           'track_id': track['track_id'],
           'track_name': track['track_name'],
@@ -67,7 +88,6 @@ class PlaylistScreen extends StatelessWidget {
           const SnackBar(content: Text('플레이리스트가 저장되었습니다!')),
         );
       } else if (response.statusCode == 200) {
-        // 중복 저장된 경우
         ScaffoldMessenger.of(context).showSnackBar(
           const SnackBar(content: Text('이미 저장된 플레이리스트입니다.')),
         );
@@ -91,9 +111,9 @@ class PlaylistScreen extends StatelessWidget {
   Future<void> deletePlaylist(BuildContext context) async {
     final url = Uri.parse('http://10.0.2.2:8000/auth/delete_playlist/');
     final body = jsonEncode({
-      'userId': userId,
-      'playlistName': playlistName,
-      'tracks': tracks.map((track) {
+      'userId': widget.userId,
+      'playlistName': _playlistName,
+      'tracks': widget.tracks.map((track) {
         return {
           'track_id': track['track_id'],
           'track_name': track['track_name'],
@@ -132,6 +152,54 @@ class PlaylistScreen extends StatelessWidget {
     }
   }
 
+  Future<void> updatePlaylistName(String newName) async {
+    final url = Uri.parse('http://10.0.2.2:8000/auth/update_playlist_name/');
+    final body = jsonEncode({
+      'userId': widget.userId,
+      'playlistName': _playlistName, // 기존 플레이리스트 이름
+      'newName': newName,
+      'tracks': widget.tracks.map((track) {
+        return {
+          'track_id': track['track_id'],
+          'track_name': track['track_name'],
+          'artist_name': track['artist_name'],
+          'album_cover': track['album_cover'],
+          'duration_ms': track['duration_ms'],
+        };
+      }).toList(),
+    });
+
+    try {
+      final response = await http.put(
+        url,
+        headers: {'Content-Type': 'application/json'},
+        body: body,
+      );
+
+      if (response.statusCode == 200) {
+        ScaffoldMessenger.of(context).showSnackBar(
+          const SnackBar(content: Text('플레이리스트 이름이 변경되었습니다.')),
+        );
+        setState(() {
+          _playlistName = newName; // 상태 업데이트
+        });
+      } else {
+        final error = jsonDecode(response.body)['error'];
+        throw Exception(error ?? '이름 변경 중 문제가 발생했습니다.');
+      }
+    } catch (e) {
+      showDialog(
+        context: context,
+        builder: (context) {
+          return AlertDialog(
+            title: const Text('오류 발생'),
+            content: Text('플레이리스트 이름 변경 중 오류가 발생했습니다: $e'),
+          );
+        },
+      );
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -152,34 +220,34 @@ class PlaylistScreen extends StatelessWidget {
         ),
         actions: [
           GestureDetector(
-                      onTap: () {
-            showMenu(
-              context: context,
-              position: const RelativeRect.fromLTRB(100, 80, 0, 0),
-              items: [
-                if (mypageflag != 1) // mypageflag가 1이 아닐 때만 "플레이리스트 저장" 버튼 추가
+            onTap: () {
+              showMenu(
+                context: context,
+                position: const RelativeRect.fromLTRB(100, 80, 0, 0),
+                items: [
+                  if (widget.mypageflag != 1)
+                    const PopupMenuItem(
+                      value: 'savePlaylist',
+                      child: Row(
+                        children: [
+                          Icon(Icons.playlist_add_rounded,
+                              color: Color(0xff777777)),
+                          SizedBox(width: 2),
+                          Text('플레이리스트 저장'),
+                        ],
+                      ),
+                    ),
                   const PopupMenuItem(
-                    value: 'savePlaylist',
+                    value: 'deletePlaylist',
                     child: Row(
                       children: [
-                        Icon(Icons.playlist_add_rounded,
-                            color: Color(0xff777777)),
+                        Icon(Icons.delete_outlined, color: Color(0xff777777)),
                         SizedBox(width: 2),
-                        Text('플레이리스트 저장'),
+                        Text('재생 목록 삭제'),
                       ],
                     ),
                   ),
-                const PopupMenuItem(
-                  value: 'deletePlaylist',
-                  child: Row(
-                    children: [
-                      Icon(Icons.delete_outlined, color: Color(0xff777777)),
-                      SizedBox(width: 2),
-                      Text('재생 목록 삭제'),
-                    ],
-                  ),
-                ),
-              ],
+                ],
               ).then((value) {
                 if (value == 'savePlaylist') {
                   savePlaylist(context);
@@ -206,23 +274,86 @@ class PlaylistScreen extends StatelessWidget {
                 decoration: BoxDecoration(
                   borderRadius: BorderRadius.circular(5),
                   color: const Color(0xff777777),
-                  image: tracks.isNotEmpty
+                  image: widget.tracks.isNotEmpty
                       ? DecorationImage(
-                          image: NetworkImage(tracks.first['album_cover']),
+                          image:
+                              NetworkImage(widget.tracks.first['album_cover']),
                           fit: BoxFit.cover,
                         )
                       : null,
                 ),
               ),
               const SizedBox(height: 25),
-              Text(
-                playlistName,
-                style: const TextStyle(
-                  fontFamily: 'Pretendard',
-                  fontSize: 18,
-                  fontWeight: FontWeight.bold,
-                  color: Color(0xff111111),
-                ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    _playlistName,
+                    style: const TextStyle(
+                      fontFamily: 'Pretendard',
+                      fontSize: 18,
+                      fontWeight: FontWeight.bold,
+                      color: Color(0xff111111),
+                    ),
+                  ),
+                  const SizedBox(width: 5),
+                  GestureDetector(
+                    onTap: () async {
+                      final newName = await showDialog<String>(
+                        context: context,
+                        builder: (BuildContext context) {
+                          String tempName = _playlistName;
+                          return AlertDialog(
+                            backgroundColor: const Color(0xffffffff),
+                            title: const Text('플레이리스트 이름 변경',
+                                style: TextStyle(
+                                  fontFamily: 'Pretendard',
+                                  fontSize: 15,
+                                  fontWeight: FontWeight.bold,
+                                  color: Color(0xff111111),
+                                )),
+                            content: TextField(
+                              onChanged: (value) {
+                                tempName = value;
+                              },
+                              controller: _textEditingController,
+                              decoration: const InputDecoration(
+                                hintText: "새 이름을 입력하세요",
+                              ),
+                              autofocus: true,
+                            ),
+                            actions: <Widget>[
+                              TextButton(
+                                onPressed: () {
+                                  Navigator.of(context).pop();
+                                },
+                                child: const Text('취소'),
+                              ),
+                              TextButton(
+                                onPressed: () {
+                                  Navigator.of(context).pop(tempName);
+                                },
+                                child: const Text('저장'),
+                              ),
+                            ],
+                          );
+                        },
+                      );
+                      
+                      if (newName != null && newName.isNotEmpty) {
+                        await updatePlaylistName(newName); // API 호출
+                        setState(() {
+                          _playlistName = newName; // 상태 업데이트
+                        });
+                      }
+                    },
+                    child: const Icon(
+                      Icons.edit_outlined,
+                      color: Color(0xff777777),
+                      size: 20,
+                    ),
+                  ),
+                ],
               ),
               const SizedBox(height: 25),
               Row(
@@ -293,7 +424,7 @@ class PlaylistScreen extends StatelessWidget {
                 ],
               ),
               const SizedBox(height: 25),
-              ...tracks.map((track) {
+              ...widget.tracks.map((track) {
                 return Column(
                   children: [
                     ListTile(
