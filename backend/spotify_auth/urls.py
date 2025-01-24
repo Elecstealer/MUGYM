b7c46349c16e1cde9cdf9dbaf4ad3a7728276c67
ParkSieun <110987950+Elecstealer@users.noreply.github.com>
2025-01-24 14:09:43 +0900

    Update urls.py

--- backend/spotify_auth/urls.py
+++ backend/spotify_auth/urls.py
@@ -12,6 +12,7 @@ urlpatterns = [
     path('get_tracks_by_type/', views.get_tracks_by_type, name='get_tracks_by_type'),
     path('save_playlist/', views.save_playlist, name='save_playlist'),
     path('delete_playlist/', views.delete_playlist, name='delete_playlist'),
+    path('update_playlist_name/', views.update_playlist_name, name='update_playlist_name'),
 
     path('user/login/', views.login_user, name='user_login'),
     path('user/register/', views.register_user, name='user_register'),
